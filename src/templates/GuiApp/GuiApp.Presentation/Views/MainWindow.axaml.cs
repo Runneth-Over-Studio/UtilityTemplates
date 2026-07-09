3d4371601bbb4079209589c00c5fe60a40e4f10b
Aaron Salisbury <aaron.salisbury@live.com>
2026-07-09 16:07:20 -0500

    Updated nuget versions. For GUI app, removed titlebar customization as with Avalonia 12 that is another beast now.

--- src/templates/GuiApp/GuiApp.Presentation/Views/MainWindow.axaml.cs
+++ src/templates/GuiApp/GuiApp.Presentation/Views/MainWindow.axaml.cs
@@ -9,22 +9,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        this.Title = AppInfo.AppDisplayName;
-
-        SetWindowMainGrid(TitleBarAndContentGrid);
-    }
-
-    public static void SetWindowMainGrid(Grid mainGrid)
-    {
-        RowDefinition titleBarRow = new() { Height = new GridLength(GetTitleBarHeight()) };
-        RowDefinition contentRow = new() { Height = GridLength.Star };
-
-        mainGrid.RowDefinitions.Add(titleBarRow);
-        mainGrid.RowDefinitions.Add(contentRow);
-    }
-
-    public static double GetTitleBarHeight()
-    {
-        return RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? 0.0D : 30.0D;
+        this.Title = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? AppInfo.AppDisplayName : string.Empty;
     }
 }
