ba698b17c08520c769a63ca961800d3c0c0a1bd8
Aaron Salisbury <aaron.salisbury@live.com>
2026-09-22 12:43:16 -0500

    Rename desktop presentation namespace

--- src/templates/GuiApp/GuiApp.Presentation/Base/Services/UILayer/Dispatcher/IAgnosticDispatcher.cs
+++ src/templates/GuiApp/GuiApp.Presentation/Base/Services/UILayer/Dispatcher/IAgnosticDispatcher.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-namespace GuiApp.Presentation.Base.Services;
+namespace GuiApp.Presentation.Desktop.Base.Services;
 
 public interface IAgnosticDispatcher
 {
