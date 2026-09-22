6974586111994387db7b0d0f949d8ea3900dabf6
Aaron Salisbury <aaron.salisbury@live.com>
2026-09-22 12:43:29 -0500

    Align desktop presentation with composition-root architecture

--- src/templates/GuiApp/GuiApp.Presentation/Models/MenuPaneItemTemplate.cs
+++ src/templates/GuiApp/GuiApp.Presentation/Models/MenuPaneItemTemplate.cs
@@ -1,5 +1,5 @@
 ﻿using System;
 
-namespace GuiApp.Presentation.Models;
+namespace GuiApp.Presentation.Desktop.Models;
 
 public record MenuPaneItemTemplate(Type ModelType, string IconKey, string Label);
